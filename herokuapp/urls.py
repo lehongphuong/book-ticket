f237e0b0463865c2e0ecfe1312dc983e8281bacd
Le Hong Phuong <helloitpdu@gmail.com>
2020-01-14 11:21:20 +0700

    PhuongLH5: commit all file at 01/14/2020 : 11:21:20.23

--- herokuapp/urls.py
+++ herokuapp/urls.py
@@ -5,12 +5,12 @@ app_name = 'home'
 urlpatterns = [
     path('', views.index, name='index'),
     # *******************************************************	
-    # begin customer	
-    path('createDatacustomer', views.createDatacustomer, name='createDatacustomer'), 	
-    path('readDatacustomer', views.readDatacustomer, name='readDatacustomer'), 	
-    path('updateDatacustomer', views.updateDatacustomer, name='updateDatacustomer'), 	
-    path('deleteDatacustomer', views.deleteDatacustomer, name='deleteDatacustomer'),  	
-    path('findDatacustomer', views.findDatacustomer, name='findDatacustomer'),  	
-    # end customer	
+    # begin Customer	
+    path('createDataCustomer', views.createDataCustomer, name='createDataCustomer'), 	
+    path('readDataCustomer', views.readDataCustomer, name='readDataCustomer'), 	
+    path('updateDataCustomer', views.updateDataCustomer, name='updateDataCustomer'), 	
+    path('deleteDataCustomer', views.deleteDataCustomer, name='deleteDataCustomer'),  	
+    path('findDataCustomer', views.findDataCustomer, name='findDataCustomer'),  	
+    # end Customer	
     # *******************************************************	 
 ]
